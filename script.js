520180fe1e3d798473217bd7b44edfae587cfc4d
Irene-nandy <irenekasiva75@gmail.com>
2020-08-02 09:47:31 +0300

    add command

---
+++ script.js
@@ -0,0 +1,11 @@
+var del = 150;
+var PizzatotalPrice = [];
+function Order (pizzaSize, crust) {
+ this.pizzaSize = pizzaSize;
+ this.crust = crust;
+ this.Topping1 = 400;
+ this.Topping2 = 1000;
+ this.pizzaPrice = 0;
+ this.pizzaNumber  = 1;
+ this.pizzaNumbers = [];
+}

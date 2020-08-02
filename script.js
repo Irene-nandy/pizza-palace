8ecdb729cd45c412a56dfea36531391807645ae5
Irene-nandy <irenekasiva75@gmail.com>
2020-08-02 09:51:58 +0300

    add fuction

--- script.js
+++ script.js
@@ -9,3 +9,33 @@ function Order (pizzaSize, crust) {
  this.pizzaNumber  = 1;
  this.pizzaNumbers = [];
 }
+Order.prototype.pizzaCost = function () {
+if (this.pizzaSize === "Small") {
+this.pizzaPrice += 1000;
+} else if (this.pizzaSize === "Medium") {
+      this.pizzaPrice += 2000;
+} else if (this.pizzaSize === "Large") {
+this.pizzaPrice += 3000;
+}
+if (this.crust === "crispy") {
+this.pizzaPrice += 1500;
+ } else if (this.crust === "stuffed") {
+this.pizzaPrice += 1200;
+} else if (this.crust === "glutenFree") {
+this.pizzaPrice += 1390;
+}
+for(var j = 1; j < this.pizzaNumbers.length; j++){
+pizzaNumber += pizzaNumbers[j];
+}
+this.pizzaPrice += this.Topping1;
+this.pizzaPrice += this.Topping2;
+this.pizzaPrice *= this.pizzaNumber;
+return this.pizzaPrice;
+ }
+Order.prototype.finalCost = function () {
+var TotalPrice = 0;
+for (var i = 0; i < PizzatotalPrice.length; i ++) {
+TotalPrice += PizzatotalPrice[i];
+}
+return TotalPrice;
+}
